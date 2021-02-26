1bf9e7bc88d8a40b5ad9956fa400aa536ea13fa3
xsthunder <xsthunder@outlook.com>
2021-02-26 12:30:43 +0800

    add resume, \n for IFS

--- m4a2mp3.sh
+++ m4a2mp3.sh
@@ -1,15 +1,22 @@
 OUTPUT_ROOT=output
 mkdir -p $OUTPUT_ROOT
+IFS=$'\n'
 set -e
 for d in `find -maxdepth 1 -type d -iname "*完全版*"`
 do
     mkdir -p $OUTPUT_ROOT/$d
     for f in `find $d -iname '*.m4a'`
     do
-		
+        dist=$OUTPUT_ROOT/`echo $f | sed -e 's/m4a/mp3/g'`
+        if test -f $dist
+        then
+            # echo skip $dist
+            continue
+        fi
 		# -vn remove cover image
 		# -q:a 8 limit rate to 48kHz
-        ffmpeg.exe -i $f -vn -q:a 8 $OUTPUT_ROOT/`echo $f | sed -e 's/m4a/mp3/g'`
+        ffmpeg.exe -i "$f" -vn -q:a 8 "$dist"
+
     done
 done
 
