eb76ba3468f73938744a2b6a9b1978885ee023be
xsthunder <xsthunder@outlook.com>
2021-02-26 11:18:02 +0800

    Create m4a2mp3.sh

---
+++ m4a2mp3.sh
@@ -0,0 +1,15 @@
+OUTPUT_ROOT=output
+mkdir -p $OUTPUT_ROOT
+set -e
+for d in `find -maxdepth 1 -type d -iname "*完全版*"`
+do
+    mkdir -p $OUTPUT_ROOT/$d
+    for f in `find $d -iname '*.m4a'`
+    do
+		
+		# -vn remove cover image
+		# -q:a 8 limit rate to 48kHz
+        ffmpeg.exe -i $f -vn -q:a 8 $OUTPUT_ROOT/`echo $f | sed -e 's/m4a/mp3/g'`
+    done
+done
+
